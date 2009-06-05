39e6ad3e32f3080995d8885d779197f5b9ea65d6
conny <conny@cad2f41c-308f-479c-8991-e92f7903365d>
2009-06-05 15:35:22 +0000

    Initial idea for an storage backend interface.
    git-svn-id: https://vcs.maemo.org/svn/conboy/trunk/conboy@173 cad2f41c-308f-479c-8991-e92f7903365d

---
+++ src/storage.h
@@ -0,0 +1,67 @@
+#ifndef STORAGE_H_
+#define STORAGE_H_
+
+#include "note.h"
+
+/**
+ * Initializes the storage backend. E.g. conntect to a database,
+ * create file paths etc.
+ * 
+ * user_data: Stuff like hostname, username, password, etc...
+ */
+void storage_initialize(gpointer user_data);
+
+/**
+ * Close all open connections, files, etc. Then free all memory that
+ * was allocated by the storage_initialize() function.
+ */
+void storage_destroy(void);
+
+/**
+ * Loads a complete note from the storage backend. The note must be freed
+ * when you're done with it.
+ */
+Note* storage_load_note(const gchar *guid);
+
+/**
+ * Only load the notes metadata, but not the content.
+ * Should at least load "title" and "last-change-date" that how
+ * it works ATM.
+ */
+Note* storage_load_note_partial(const gchar *guid);
+
+/**
+ * Adds the content to a existing partial note. 
+ * Returns TRUE if successfull.
+ */
+gboolean storage_load_note_content(Note *note);
+
+/**
+ * Saves a note into the storage backend. The containing GUID is used
+ * as identifier.
+ * Returns TRUE if successfull.
+ */
+gboolean storage_save_note(Note *note);
+
+/**
+ * Returns a list of gchar* GUIDs which can be used
+ * to load a note.
+ * Free the list and the containing strings if you're done.
+ */
+GList* storage_get_all_note_ids();
+
+/**
+ * Deletes a note with the identifier "guid" from the storage backend.
+ * Returns: TRUE when successfull, FALSE otherwise.
+ */
+gboolean storage_delete_note(const gchar *guid);
+
+/**
+ * Returns the time in seconds that pass between a note modification and the
+ * actual saving.
+ * Note: Tomboy uses 4 seconds with its xml backend.
+ */ 
+gint storage_get_save_interval(); 
+
+
+#endif /*STORAGE_H_*/
