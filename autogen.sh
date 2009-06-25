a5fd472f30a501c444b3774dcef92947d80d011f
conny <conny@cad2f41c-308f-479c-8991-e92f7903365d>
2009-06-25 07:16:14 +0000

    Switched to automake 1.9 because 1.8 is not available on recent ubuntu installations. Only 1.4 / 1.7 / 1.9
    git-svn-id: https://vcs.maemo.org/svn/conboy/trunk/conboy@207 cad2f41c-308f-479c-8991-e92f7903365d

--- autogen.sh
+++ autogen.sh
@@ -5,8 +5,8 @@ glib-gettextize --copy --force
 libtoolize --automake
 intltoolize --copy --force --automake
 
-aclocal-1.8
+aclocal-1.9
 autoconf
 autoheader
-automake-1.8 --add-missing --foreign
+automake-1.9 --add-missing --foreign
 ./configure --enable-maintainer-mode
