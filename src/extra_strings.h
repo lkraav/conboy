9f14fc02e264fc72b9341fd171b1b4c5bf772ab8
conny <conny@cad2f41c-308f-479c-8991-e92f7903365d>
2010-06-01 18:47:46 +0000

    Updated translations. Introduced .plugin.desktop.in files again. Otherwise they are ignored by "make po-update".
    git-svn-id: https://vcs.maemo.org/svn/conboy/trunk/conboy@703 cad2f41c-308f-479c-8991-e92f7903365d

---
+++ src/extra_strings.h
@@ -0,0 +1,5 @@
+
+/* Translators: First line of description used by the package manager */
+#define package_description_short _("Conboy is a note taking application.");
+/* Translators: Following lines of description used by the package manager */
+#define package_description_long  _("It is designed to be easy to use - yet powerful. Conboy can read and write the Tomboy file format to be compatible with Tomboy on Linux, Mac and Windows.");
